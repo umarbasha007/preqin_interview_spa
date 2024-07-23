c18a5aa26252e5cbca37b4b3ce5dcef56d0e3bd7
Umar Basha - Tech Monk <umarbasha007@gmail.com>
2024-07-24 03:12:07 +0530

    Route based code splitting

--- preqin-spa/src/App.tsx
+++ preqin-spa/src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import InvestorTable from "./components/InvestorTable";
-import InvestorDetails from "./components/InvestorDetails";
+
+// import InvestorTable from "./components/InvestorTable";
+// import InvestorDetails from "./components/InvestorDetails";
+
+// Lazy load the components
+const InvestorTable = lazy(() => import("./components/InvestorTable"));
+const InvestorDetails = lazy(() => import("./components/InvestorDetails"));
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<InvestorTable />} />
-        <Route path="/investors/:investorId" element={<InvestorDetails />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<InvestorTable />} />
+          <Route path="/investors/:investorId" element={<InvestorDetails />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
